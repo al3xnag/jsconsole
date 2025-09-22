b348c923c1a682007161cf51c457b71dafe3c7b3
Aleksei Nagovitsyn <al3x.nag@gmail.com>
2025-09-22 23:00:51 +0500

    Fix empty test files

---
+++ packages/interpreter/src/nodes/BreakStatement.test.ts
@@ -0,0 +1,3 @@
+import { it } from '../test-utils'
+
+it(`for (var i = 0; i < 3; i++) { break; throw 1; }`, undefined)
---
+++ packages/interpreter/src/nodes/ContinueStatement.test.ts
@@ -0,0 +1,3 @@
+import { it } from '../test-utils'
+
+it(`for (var i = 0; i < 3; i++) { continue; throw 1; }`, undefined)
